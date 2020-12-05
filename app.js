61b57da09707f8aa891a3da03c931bc1aeecc8bd
Le Quang Bao <quangbao1712@gmail.com>
2020-12-05 23:12:06 +0700

    30% javascript

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const game = () => {
   let pScore = 0;
   let cScore = 0;
 
+
+  //Start Game
   const startGame = () => {
     const playBtn = document.querySelector(".intro button");
     const introScreen = document.querySelector(".intro");
@@ -9,8 +11,23 @@ const game = () => {
 
     playBtn.addEventListener("click", () => {
       introScreen.classList.add("fadeOut");
+      matchScreen.classList.add("fadeIn");
     });
   };
+
+  //Play Match
+
+  const playGame = () => {
+    const options = document.querySelectorAll('options button');
+    const playerHand = document.querySelector('.player-hand');
+    const computerHand = document.querySelector('.computer-hand');
+
+
+    //Computer Options
+    
+    const computerOptions = ['rock', 'paper', 'scissors'];
+
+  }
 
   console.log('game')
   //Call all inner function
